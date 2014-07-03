27a300c1b5547469a95dbb03247c3adca5a7e91e
Robin Hübner <profan@pagefault.se>
2014-07-03 17:54:00 +0200

    Fixed license notice to include name and email.

--- brain.h
+++ brain.h
@@ -1,7 +1,7 @@
 /*
 * The MIT License (MIT)
 *
-* Copyright (c) 2014 Profan
+* Copyright (c) 2014 Robin Hübner <robinhubner@gmail.com>
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
